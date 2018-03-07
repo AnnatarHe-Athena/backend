7e2a68b64d9ef352bfbff84b839b4b288399c74d
AnnatarHe <iamhele1994@gmail.com>
2018-03-07 20:30:47 +0800

    remove release config

--- cfg/cfg.release.go
+++
@@ -1,8 +0,0 @@
-// +build release
-
-package cfg
-
-var CONFIG = &ConfigType{
-	IsDev:               false,
-	DatabaseResourceStr: "host=db user=postgres password=admin dbname=postgres sslmode=disable",
-}
